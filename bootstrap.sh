4720ffffde04c6383716b56690e941cabd716154
Blake Knight <oss.ideas@gmail.com>
2015-09-01 15:37:42 -0500

    See if bootstrap has ran before and check local repository for cleanliness

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,25 +1,44 @@
 #!/bin/bash
 
 export basedir=$(cd $(dirname "$0") && pwd)
+export last_bootstrap_file="${basedir}/.last-bootstrap"
 
 source $basedir/mylog.sh
 source $basedir/install_functions.sh
 
+# Basic information and setup
 pheader 'Checking the essentials...'
+
 piinfo "Operating System: "
 echo $OS_NAME
 
-piinfo 'Node.js: '
-is_installed node nodejs || install_node
-
-piinfo 'npm: '
-is_installed npm || install_npm
-
-piinfo 'A fake test program: '
-is_installed fake fake2 || install_fake
-
-if $(is_clean_directory upgrade); then
-	piinfo 'Ready to update'
+piinfo 'Checking for previous bootstrap: '
+if [[ -f $last_bootstrap_file ]]; then
+	last_update_time=$(cat $last_bootstrap_file)
+	pinfo $(timestamp2pretty $last_update_time)
 else
-	pwarn 'Please clean up directory'
+	pinfo 'none found'
 fi
+
+piinfo 'Checking this repo: '
+if $(require_clean_directory upgrade); then
+	piinfo 'ready to bootstrap'
+else
+	pwarn 'Please clean up directory (e.g. `git checkout -- .`) and try again'
+	exit 2
+fi
+
+# Get latest version of files
+pheader 'Checking for file updates...'
+
+git fetch --progress
+new_updates_available=`git diff HEAD FETCH_HEAD`
+if [ "$new_updates_available" != "" ]; then
+	pinfo 'Updating to latest version...'
+
+	git checkout -- .
+	git checkout master
+	git pull --progress
+fi
+
+# date +%s > last_update_file
