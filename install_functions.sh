13c23709c1deaccea260584169bc6928bdc71494
Blake Knight <oss.ideas@gmail.com>
2015-09-01 14:59:39 -0500

    Add function to check for clean git directory

--- install_functions.sh
+++ install_functions.sh
@@ -18,6 +18,32 @@ is_installed() {
 	}
 }
 
+# Ensure there are no uncommitted/unstaged changes in a git repo before working on it
+require_clean_directory() {
+	# Update the index
+	git update-index -q --ignore-submodules --refresh
+	err=0
+
+	# Disallow unstaged changes in the working tree
+	if ! $(git diff-files --quiet --ignore-submodules --); then
+		perror >&2 "cannot $1: you have unstaged changes"
+		git diff-files --name-status -r --ignore-submodules -- >&2
+		err=1
+	fi
+
+	# Disallow uncommitted changes in the index
+	if ! $(git diff-index --cached --quiet HEAD --ignore-submodules --); then
+		perror >&2 "cannot $1: your index contains uncommitted changes"
+		git diff-index --cached --name-status -r --ignore-submodules HEAD -- >&2
+		err=1
+	fi
+
+	if [ $err = 1 ]; then
+		perror >&2 'Please commit or stash them'
+		exit 1
+	fi
+}
+
 # Installs Homebrew for OS X
 install_brew() {
 	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
