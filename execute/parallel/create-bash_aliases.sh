73f4930fa2fb9a51c4bceb2ee05ba0604be53617
Blake Knight <oss.ideas@gmail.com>
2015-09-02 12:54:35 -0500

    Initial working version of .bash_aliases creation script

--- execute/parallel/create-bash_aliases.sh
+++ execute/parallel/create-bash_aliases.sh
@@ -1,25 +1,39 @@
 fileName="$HOME/.bash_aliases"
+source $libdir/mylog.sh
+source $basedir/install_functions.sh
 
-echo 'Got OS type: ' $OS_NAME
+if [ -f $fileName ]; then
+	mv $fileName ${fileName}.backup
+fi
 
-# if [ -f $fileName ]; then
-# 	mv $fileName ${fileName}.backup
-# fi
+cat <<-EOF > $fileName
+	# Reasonable defaults
+	alias ll='ls -lhFS'
+	alias la='ls -A'
+	alias l='ls -CF'
+EOF
 
-# cat <<-EOF > $fileName
-# 	# Reasonable defaults
-# 	alias ll='ls -lhFS'
-# 	alias la='ls -A'
-# 	alias l='ls -CF'
+is_installed poweroff > /dev/null || {
+	cat <<-EOF >> $fileName
+		alias poweroff='sudo shutdown -h now'
+		alias reboot='sudo reboot'
 
-# 	# Add colors to common programs
-# 	alias grep='grep --colour=auto'
-# 	alias fgrep='fgrep --colour=auto'
-# 	alias egrep='egrep --colour=auto'
+	EOF
+}
 
-# 	alias poweroff='sudo shutdown -h now'
-# 	alias reboot='sudo reboot'
+echo '# Add colors to common programs' >> $fileName
+cat <<-EOF >> $fileName
+	alias grep='grep --colour=auto'
+	alias fgrep='fgrep --colour=auto'
+	alias egrep='egrep --colour=auto'
 
-# 	alias firefox='/Applications/FirefoxDeveloperEdition.app/Contents/MacOS/firefox'
-# 	alias imageoptim='/Applications/ImageOptim.app/Contents/MacOS/ImageOptim'
-# EOF
+EOF
+
+echo '# Setup shortcuts to executables in non-standard places' >> $fileName
+if [ -x '/Applications/FirefoxDeveloperEdition.app/Contents/MacOS/firefox' ]; then
+	echo "alias firefox='/Applications/FirefoxDeveloperEdition.app/Contents/MacOS/firefox'" >> $fileName
+fi
+
+if [ -x '/Applications/ImageOptim.app/Contents/MacOS/ImageOptim' ]; then
+	echo "alias imageoptim='/Applications/ImageOptim.app/Contents/MacOS/ImageOptim'" >> $fileName
+fi
