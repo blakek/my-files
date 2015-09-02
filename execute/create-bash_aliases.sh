049bc44270a681761b3003b1aebfc6551a927cdc
Blake Knight <oss.ideas@gmail.com>
2015-09-02 12:21:34 -0500

    Initial add of creating bash aliases/profile files (not yet working)

---
+++ execute/create-bash_aliases.sh
@@ -0,0 +1,25 @@
+fileName="$HOME/.bash_aliases"
+
+echo 'Got OS type: ' $OS_NAME
+
+# if [ -f $fileName ]; then
+# 	mv $fileName ${fileName}.backup
+# fi
+
+# cat <<-EOF > $fileName
+# 	# Reasonable defaults
+# 	alias ll='ls -lhFS'
+# 	alias la='ls -A'
+# 	alias l='ls -CF'
+
+# 	# Add colors to common programs
+# 	alias grep='grep --colour=auto'
+# 	alias fgrep='fgrep --colour=auto'
+# 	alias egrep='egrep --colour=auto'
+
+# 	alias poweroff='sudo shutdown -h now'
+# 	alias reboot='sudo reboot'
+
+# 	alias firefox='/Applications/FirefoxDeveloperEdition.app/Contents/MacOS/firefox'
+# 	alias imageoptim='/Applications/ImageOptim.app/Contents/MacOS/ImageOptim'
+# EOF
